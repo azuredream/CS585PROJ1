642f55b47292f3c807a96bcef6e161aac8338d5e
Mahdi Alouane <malouane@wpi.edu>
2019-09-17 16:33:22 -0400

    Query4: Changed logic and added output display

--- MapReduce/Query4/Query4.java
+++ MapReduce/Query4/Query4.java
@@ -17,80 +17,94 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import java.net.URI;
 
+public class Query4 {
 
-public class Query4{
+	public static class CountryMapper extends Mapper<Object, Text, Text, FloatWritable> {
 
-	public static class CountryMapper extends Mapper<Object, Text, Text, FloatWritable>{
-		
-		private HashMap<Integer, Integer> country_to_Id = new HashMap<Integer,Integer>();
+		private HashMap<Integer, Integer> custCountryMap = new HashMap<Integer, Integer>();
+		private HashMap<Integer, Integer> countryCustNumMap = new HashMap<Integer, Integer>();
 
-	    public void setup(Context context) throws IOException{
-	    	String[] tokens;
-	    	Configuration conf = context.getConfiguration();
-	    	URI[] uris = DistributedCache.getCacheFiles(conf);
-	    	FSDataInputStream dataIn = FileSystem.get(context.getConfiguration()).open(new Path(uris[0]));
-	    	BufferedReader br = new BufferedReader(new InputStreamReader(dataIn));
-	    	String line = br.readLine();
-	    	while (line != null){
-	    		tokens = line.split(",");
-	            int key = Integer.parseInt(tokens[0]);
-	            int countrycode = Integer.parseInt(tokens[4]);
-	            country_to_Id.put(key, countrycode);
-	    		line = br.readLine();
-	    	}
-	    }
-		
-	    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {                           
+		public void setup(Context context) throws IOException {
+			Configuration conf = context.getConfiguration();
+			URI[] uris = DistributedCache.getCacheFiles(conf);
+			FSDataInputStream dataIn = FileSystem.get(conf).open(new Path(uris[0]));
+			BufferedReader br = new BufferedReader(new InputStreamReader(dataIn));
+			String line = br.readLine();
+
+			while (line != null) {
+				String[] tokens = line.split(",");
+
+				int key = Integer.parseInt(tokens[0]);
+				int countrycode = Integer.parseInt(tokens[4]);
+				custCountryMap.put(key, countrycode);
+
+				int count = countryCustNumMap.containsKey(countrycode) ? countryCustNumMap.get(countrycode) : 0;
+				countryCustNumMap.put(countrycode, count + 1);
+				
+				line = br.readLine();
+			}
+		}
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 			String[] tokens = value.toString().split(",");
-			Text Countrycode = new Text();
-			FloatWritable TransTotal = new FloatWritable();
+
+			Text countryCodeKey = new Text();
+			FloatWritable transTotal = new FloatWritable();
+
 			int joinID = Integer.parseInt(tokens[1]);
-			Countrycode.set(country_to_Id.get(joinID)+"");
-			TransTotal.set(Float.parseFloat(tokens[2]));
-	        context.write(Countrycode, TransTotal);
-	    }
+			int countryCode = custCountryMap.get(joinID);
+			int custNumber = countryCustNumMap.get(countryCode);
+
+			countryCodeKey.set(String.format("%d,%d", countryCode, custNumber));
+			transTotal.set(Float.parseFloat(tokens[2]));
+			context.write(countryCodeKey, transTotal);
+		}
 	}
 
-	public static class FilterRecuder extends Reducer<Text, FloatWritable, Text, Text>{
-	    public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException{
-	    	int counter = 0;
-	        float max = Integer.MIN_VALUE;
-	        float min = Integer.MAX_VALUE;
-	                                               
-	        for (FloatWritable f : values) {
-	        	counter += 1;
-	            float tmpval = f.get();
-	            if (tmpval > max) max = tmpval;
-	            if (tmpval < min) min = tmpval;
-	            		
-	        }
-	        context.write(key, new Text(String.format("%d,%f,%f",counter,min,max)));
-	    }
-	}
-	
-	
-	public static void main(String[] args) throws Exception{
+	public static class FilterRecuder extends Reducer<Text, FloatWritable, Text, Text> {
+		public void reduce(Text key, Iterable<FloatWritable> values, Context context)
+				throws IOException, InterruptedException {
+			float max = Integer.MIN_VALUE;
+			float min = Integer.MAX_VALUE;
 
-        long start = new Date().getTime();
-	    Configuration conf = new Configuration();
-	    conf.set("mapred.jop.tracker", "hdfs://localhost:8020");
-	    conf.set("fs.default.name", "hdfs://localhost:8020");
-	    DistributedCache.addCacheFile(new URI("hdfs://localhost:8020/user/hadoop/data/Customers"), conf);
-	    Job job = new Job(conf, "Filter");
-	    
-	    job.setJarByClass(Query4.class);
-	    
-	    job.setMapOutputKeyClass(Text.class);
-	    job.setMapOutputValueClass(FloatWritable.class); 
-	    job.setMapperClass(CountryMapper.class);
-	    job.setReducerClass(FilterRecuder.class);                        
-	    job.setOutputKeyClass(Text.class);
-	    job.setOutputValueClass(Text.class);
-	    FileInputFormat.addInputPath(job, new Path(args[0]));
-	    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	    job.waitForCompletion(true);
+			for (FloatWritable f : values) {
+				float currTransTotal = f.get();
+				if (currTransTotal > max)
+					max = currTransTotal;
+				if (currTransTotal < min)
+					min = currTransTotal;
+			}
+
+			context.write(key, new Text(String.format("%f,%f", min, max)));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		long start = new Date().getTime();
+		Configuration conf = new Configuration();
+		conf.set("mapred.jop.tracker", "hdfs://localhost:8020");
+		conf.set("fs.default.name", "hdfs://localhost:8020");
+		DistributedCache.addCacheFile(new URI("hdfs://localhost:8020/user/hadoop/data/Customers"), conf);
+		Job job = new Job(conf, "Filter");
+
+		job.setJarByClass(Query4.class);
+
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(FloatWritable.class);
+
+		job.setMapperClass(CountryMapper.class);
+		job.setReducerClass(FilterRecuder.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
 		long end = new Date().getTime();
-		System.out.println(String.format("Job executed in %d seconds", (end - start) / 1000));  
+		System.out.println(String.format("Job executed in %d seconds", (end - start) / 1000));
 	}
 
 }
--- MapReduce/Query4/start.sh
+++ MapReduce/Query4/start.sh
@@ -4,3 +4,4 @@ javac -classpath /usr/share/hadoop/hadoop-core-1.2.1.jar -d query4_classes ./Que
 jar -cvf ./query4.jar -C query4_classes/ .
 hadoop dfs -rmr /user/hadoop/output
 hadoop jar query4.jar Query4 /user/hadoop/data/Transactions /user/hadoop/output
+hadoop dfs -cat /user/hadoop/output/* | sort -n
